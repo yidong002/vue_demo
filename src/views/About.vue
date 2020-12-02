9dc577f5731c5401e04d6dcda00aecc45bc7363a
xuwei <1098404803@qq.com>
2020-12-02 13:55:14 +0800

    vue_demo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul class="infinite-list" v-infinite-scroll="load" style="overflow:auto">
+      <li v-for="i in count" :key="i" class="infinite-list-item">{{ i }}</li>
+    </ul>
   </div>
 </template>
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      count:10
+    }
+  },
+  methods: {
+    load () {
+      // this.count += 10
+    }
+  }
+}
+</script>
